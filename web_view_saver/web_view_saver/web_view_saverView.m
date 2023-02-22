51e74b4861e1039bad3836f703545f6867805ee5
Igor <igorvm@gmail.com>
2023-02-22 09:42:03 -0500

    Configure the URL via plist

--- web_view_saver/web_view_saver/web_view_saverView.m
+++ web_view_saver/web_view_saver/web_view_saverView.m
@@ -12,16 +12,36 @@
 {
     WKWebView *webView;
     bool loaded;
+    FILE* log_file;
 }
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
+        
         [self setAnimationTimeInterval:1/30.0];
-        URL = [NSURL URLWithString:@"https://imandr.github.io/attractors/saver.html"];
+        NSString *url = NULL;
+        
+        // get URL from my bundle
+        NSArray <NSBundle*> *bundles = [NSBundle allBundles];
+        long nbundles = [bundles count];
+        int i;
+        for( i = 0; i < nbundles; i++ )
+        {
+            NSBundle* b = [bundles objectAtIndex:i];
+            if( [b.bundlePath containsString:@"web_view_saver"] )
+            {
+                url = [b objectForInfoDictionaryKey:@"SaverURL"];
+                break;
+            }
+        }
+
+        [self log:url];
+        URL = [NSURL URLWithString:url];
         NSURLRequest *request = [NSURLRequest requestWithURL: URL];
         WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+        config.websiteDataStore = [WKWebsiteDataStore nonPersistentDataStore];
         webView = [[WKWebView alloc] initWithFrame:frame configuration:config];
         webView.navigationDelegate = self;
         [webView loadRequest:request];
@@ -32,6 +52,8 @@
 - (void) webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
 {
     [self addSubview:webView];
+    [self setAutoresizesSubviews:YES];
+    [webView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
     [self setNeedsDisplay:YES];
 }
 
@@ -65,5 +87,15 @@
 {
     return nil;
 }
+
+- (void) log:(NSString*) message
+{
+    if( log_file != nil )
+    {
+        fprintf(log_file, "log: %s\n", message.UTF8String);
+        fflush(log_file);
+    }
+}
+
 
 @end
